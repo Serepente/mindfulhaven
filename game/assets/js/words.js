e45ff1fc20d30c436b397f8d2a72148a0ef1db54
Serepente <velosodonallen@gmail.com>
2024-05-12 13:22:30 +0800

    Welcome to Mindful Haven

---
+++ game/assets/js/words.js
@@ -0,0 +1,42 @@
+let words = [
+    {
+        word: "Chronic stress",
+        hint: "Weaken the immune system, increase the risk of illness, and exacerbate existing health conditions."
+    },
+    {
+        word: "Connections",
+        hint: "Maintaining healthy social _ plays a crucial role in students mental health."
+    },
+    {
+        word: "Performance",
+        hint: "Influences students' ability to concentrate, retain information, and perform well academically."
+    },
+    {
+        word: "Challenges",
+        hint: "With the right support and resources, students can navigate these more effectively"
+    },
+    {
+        word: "Development",
+        hint: "College is not just about academic learning; it's also a time for personal growth and _"
+    },
+    {
+        word: "Resilience",
+        hint: "The ability to bounce back from setbacks or difficulties"
+    },
+    {
+        word: "Interconnected",
+        hint: "How different aspects or elements are connected or related to each other"
+    },
+    {
+        word: "Fulfillment",
+        hint: "Achieving long-term satisfaction."
+    },
+    {
+        word: "Awareness",
+        hint: "Recognizing mental health importance."
+    },
+    {
+        word: "Balance",
+        hint: "Harmony between mind and body."
+    }
+]
